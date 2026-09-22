5a087562ae0f32b1ca827146669ba086cbb18987
Alejandrino <aleja@example.com>
2026-09-22 14:30:33 +0800

    fix(router): add _redirects for Cloudflare Pages and support SPA deep-linking and HEAD in local dev server

--- scripts/serve_spa.py
+++ scripts/serve_spa.py
@@ -46,6 +46,23 @@ class SpaRequestHandler(SimpleHTTPRequestHandler):
         self.path = '/index.html'
         return super().do_GET()
 
+    def do_HEAD(self):
+        url_path = self.path.split('?')[0].split('#')[0]
+        clean_rel = url_path.lstrip('/')
+        local_path = os.path.join(ROOT_DIR, clean_rel)
+
+        if os.path.exists(local_path) and not os.path.isdir(local_path):
+            return super().do_HEAD()
+        if os.path.isdir(local_path) and os.path.exists(os.path.join(local_path, 'index.html')):
+            return super().do_HEAD()
+
+        _, ext = os.path.splitext(url_path)
+        if ext and ext not in ['.html', '.htm']:
+            return super().do_HEAD()
+
+        self.path = '/index.html'
+        return super().do_HEAD()
+
 def run(port=PORT):
     server_address = ('', port)
     try:
